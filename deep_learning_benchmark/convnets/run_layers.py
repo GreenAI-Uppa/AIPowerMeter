aaec7f72cdd251883c718444c8c87b09d0e99483
Loustau <sloustau@lumenai.fr>
2021-10-26 12:31:07 +0200

    convnet study

---
+++ deep_learning_benchmark/convnets/run_layers.py
@@ -0,0 +1,51 @@
+import time, os
+import numpy as np
+from deep_learning_power_measure.power_measure import experiment, parsers
+
+import nets
+import torch
+
+#Create your favorites CNN
+channels = 3
+nb_filters = 16
+nb_conv_layers_list = [2,4,6]
+
+#choose your favorite device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using {} device'.format(device))
+
+#generate a random image
+image_size = 224
+image_test = torch.rand(1,3,image_size,image_size)
+image_test = image_test.to(device)
+
+
+for u,input_size in enumerate(input_sizes):
+    #load the model to the device
+    nb_conv_layers = input_size
+    model = nets.ConvNet(channels,nb_filters,nb_conv_layers)
+    model = model.to(device)
+    iters = int(40000/(u+1))#number of inferences
+    print(iters,'inferences')
+    xps = 10#number of experiments to reach robustness
+    for k in range(xps):
+        print('Experience',k+1,'/',xps,'is running')
+        latencies = []
+        #AIPM
+        driver = parsers.JsonParser(os.path.join(os.getcwd(),"/data/convnets/input_"+str(input_size)+"/run_"+str(k)))
+        exp = experiment.Experiment(driver)
+        p, q = exp.measure_yourself(period=2)
+        start_xp = time.time()
+        for t in range(iters):
+            #print(t)
+            start_iter = time.time()
+            y = model(image_test)
+            res = time.time()-start_iter
+            #print(t,'latency',res)
+            latencies.append(res)
+        q.put(experiment.STOP_MESSAGE)
+        end_xp = time.time()
+        print("power measuring stopped after",end_xp-start_xp,"seconds for experience",k+1,"/",xps)
+        driver = parsers.JsonParser("/data/convnets/input_"+str(input_size)+"/run_"+str(k))
+        #write latency.csv next to power_metrics.json file
+        np.savetxt("/data/convnets/input_"+str(input_size)+"/run_"+str(k)+"/latency.csv",np.array(latencies))
