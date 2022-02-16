f1cb913d053f7ee27f312a9013874a2738f32445
paul <>
2022-02-16 15:27:37 +0100

    correcting print in the r script

--- examples/matmul_r.m
+++ examples/matmul_r.m
@@ -1,7 +1,8 @@
 #! /usr/bin/Rscript
-for(n in 1:500){
+niter=500
+for(n in 1:niter){
     if((n%%10)==0){
-        print(sprintf("%i iterations over %i",n,1000))
+        print(sprintf("%i iterations over %i",n,niter))
         }
     A<-matrix(rnorm(100000),nrow=100)
     B<-matrix(rnorm(100000),nrow=1000)
