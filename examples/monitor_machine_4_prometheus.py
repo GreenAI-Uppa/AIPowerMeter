07690081760cd7642ffd2484a8a4d3e495b7c5ae
paul <>
2023-06-28 15:42:44 +0200

    adding example with training phases

--- examples/monitor_machine_4_prometheus.py
+++ examples/monitor_machine_4_prometheus.py
@@ -3,3 +3,4 @@ from deep_learning_power_measure.power_measure import experiment, prometheus_cli
 driver = prometheus_client.PrometheusClient()
 exp = experiment.Experiment(driver)
 exp.monitor_machine(period=5)
+
