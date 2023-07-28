4b926d5bff853a3188278fb0cb34b064ec701ba6
paul <>
2023-07-28 10:21:02 +0200

    adding start and end to duration

--- examples/read_json_results.py
+++ examples/read_json_results.py
@@ -22,8 +22,10 @@ args = parser.parse_args()
 
 driver = parsers.JsonParser(args.output_folder)
 exp_result = experiment.ExpResults(driver)
+exp_result.print()
 if args.metric_name is None:
-    print('metric_name argument empty please provide a value')
+    print()
+    print('metric_name argument empty please provide a value to also plot graphs')
     print()
     print(exp_result)
 else:
