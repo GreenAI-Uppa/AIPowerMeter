ef272b2677d4c82115c5ea2c254ed9d37fd8b307
gay <gay@slurm.lab-ia.local>
2023-12-19 17:50:47 +0100

    adding scripts

--- scripts_labia/stats_per_user_per_state.py
+++ scripts_labia/stats_per_user_per_state.py
@@ -1,10 +1,15 @@
 import datetime
-from deep_learning_power_measure.power_measure import experiment, parsers
 from deep_learning_power_measure import labia
+import argparse
+
+parser = argparse.ArgumentParser("Display statistics for each user and each slurm job state")
+parser.add_argument('--start',help='start date in isoformat',default='2023-11-16')
+parser.add_argument('--end',help='end date in isoformat',default='2023-11-17')
+args = parser.parse_args()
 
 
-start = datetime.datetime(2023, 11, 7).timestamp()
-end = datetime.datetime(2023, 11, 19).timestamp()
+start = datetime.datetime.fromisoformat(args.start).timestamp()
+end = datetime.datetime.fromisoformat(args.end).timestamp()
 
 ## display per user
 all_user_stats = labia.get_stats_all_users(start, end)
@@ -17,5 +22,5 @@ labia.print_summaries(summaries)
 per_status = labia.get_stats_per_job_status(all_user_stats)
 per_status_summaries = {}
 for user, user_stats in per_status.items():
-    summaries[user] = labia.summarize(user_stats) 
+    per_status_summaries[user] = labia.summarize(user_stats) 
 labia.print_summaries(per_status_summaries)
